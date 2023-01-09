821037bc81d7ee88a912437712b0629d5404e906
mwblevins <michaelwblevins@gmail.com>
2023-01-09 21:32:48 +0000

    fizz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    ourprint = ""
+    for x in range(1, 100):
+        ourprint = ""
+        if (x % 3 == 0):
+            ourprint += "Fizz"
+        if (x % 5 == 0):
+            ourprint += "Buzz"
+        if (ourprint == ""):
+            ourprint = "{}".format(x)
+        print(ourprint.format(), end=" ")
