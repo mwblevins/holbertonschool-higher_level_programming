5623879d9937242a6feaaf2c177eca3f1ed495e7
mwblevins <michaelwblevins@gmail.com>
2023-01-15 18:50:20 +0000

    adding tuples is fun

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -2,11 +2,11 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    ay = [0, 0]
-    be = [0, 0]
+    val_a = [0, 0]
+    val_b  = [0, 0]
     for i in range(0, 2):
         if (len(tuple_a) > i):
-            ay[i] = tuple_a[i]
-        if (len(tiuple_b) > i):
-            be[i] = tuple_b[i]
-    return tuple((ay[0] + be[0], ay[1] + be[1]))
+            val_a[i] = tuple_a[i]
+        if (len(tuple_b) > i):
+            val_b[i] = tuple_b[i]
+    return tuple((val_a[0] + val_b[0], val_a[1] + val_b[1]))
