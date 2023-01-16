5dd0fa11935db8f1a4bdc79dcfe4107934e399bd
mwblevins <michaelwblevins@gmail.com>
2023-01-16 23:22:29 +0000

    new

--- 0x03-python-more_data_structures/7-update_dictionary.py
+++ 0x03-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    update = {**a_dictionary, key: value}
+    new_key = key
+    new_value = value
+    update = {**a_dictionary, new_key: new_value}
     print(update)
