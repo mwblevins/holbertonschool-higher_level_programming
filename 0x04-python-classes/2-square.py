54e9c47317d2b90d7a02db1f3fa514f04ac40eb2
mwblevins <michaelwblevins@gmail.com>
2023-01-17 23:56:09 +0000

    att size

---
+++ 0x04-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Class Documentation"""
+
+
+class Square:
+    """Class square with private size"""
+    pass
+
+
+def __init__(self, size=0):
+    """init method of class Square
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
+"""Return area of a square"""
+
+
+def area(self):
+    return (self.__size * self.__size)
