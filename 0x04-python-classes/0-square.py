b784f447fc51f063f473ac2a2020f392cb7e96d9
mwblevins <michaelwblevins@gmail.com>
2023-01-17 23:08:46 +0000

    doc

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""this is a square"""
+"""Class this is a simple Square"""
 
 
 class Square:
     """Class that represents a square
     """
     def __init__(self, size):
-        """init method
-
-        Args:
-            size (int, optional): _description_. Defaults to 0.
-        """
+        """init method"""
     self.___size = size
