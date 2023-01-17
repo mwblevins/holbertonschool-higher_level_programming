e96009bc97ad1441ec2228aee7f7cae2edc1f8e2
mwblevins <michaelwblevins@gmail.com>
2023-01-17 23:26:22 +0000

    size exceptions

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This is for the size of the square i believe"""
+
+
+class Square:
+    """Defines a class Square"""
+
+    def __init__(self, size=0):
+        """init method of class Square"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._-size = size
