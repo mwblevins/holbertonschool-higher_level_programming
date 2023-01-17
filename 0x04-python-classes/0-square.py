9fd01cd4cf82da3a553dcdd68e61f9fb166d3bce
mwblevins <michaelwblevins@gmail.com>
2023-01-17 22:49:56 +0000

    new directory, readme and 0

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square Project for Holberton
+
+Is a Module that contains a class that is a Square.
+"""
+
+
+class Square:
+    """Class that represents a square
+    """
+    def __init__(self, size=0):
+        """init method
+
+        Args:
+            size (int, optional): _description_. Defaults to 0.
+        """
+    self.___size = size
