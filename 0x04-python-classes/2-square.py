82033a52fd903b41b9438e86ce2089314ee3e3a5
mwblevins <michaelwblevins@gmail.com>
2023-01-18 00:07:00 +0000

    size area

--- 0x04-python-classes/2-square.py
+++ 0x04-python-classes/2-square.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 
-"""Class Documentation"""
+"""Class documentation"""
 
 
 class Square:
-    """Class square with private size"""
+    """class Square with private size
+    """
     pass
 
-
-def __init__(self, size=0):
-    """init method of class Square
+    def __init__(self, size=0):
+        """ init method of class Square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
     """
-    if (not isinstance(_size, int)):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = size
-"""Return area of a square"""
-
-
-def area(self) -> int:
-    return self.__size ** 2
+    Return the area
+    """
+    def area(self):
+        return self.__size ** 2
