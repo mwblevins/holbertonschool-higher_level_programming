7c6a4b27fca980f1a64add869ab8483ccc83871d
mwblevins <michaelwblevins@gmail.com>
2023-01-29 00:28:41 +0000

    5

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+def is_same_class(obj, a_class):
+    """same class"""
+    return (type(obj) is a_class)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Task 5"""
+
+
+class BaseGeometry():
+    """geo class"""
+    pass
