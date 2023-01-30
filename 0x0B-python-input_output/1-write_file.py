2b35f6c01c2362d03ec6737064d9949e395dd89d
mwblevins <michaelwblevins@gmail.com>
2023-01-30 00:12:10 +0000

    forgot doc

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,7 @@
 
 
 def write_file(filename="", text=""):
+    """Forgot"""
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
-        return len(text)
+    return len(text)
