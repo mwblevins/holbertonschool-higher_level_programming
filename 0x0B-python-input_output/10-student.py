9bc22773967c908120b396b7f678d0ed99816fc2
mwblevins <michaelwblevins@gmail.com>
2023-01-30 01:38:44 +0000

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Task 10"""
+
+class Student:
+    """again with the docs"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns description"""
+        
+        if attrs is None:
+            return (vars(self))
+        ats = {}
+        for key, value in vars(self).items():
+            if key in ats:
+                ats[key] = value
+        return (ats)
