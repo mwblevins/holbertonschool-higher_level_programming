60e92a1c5121992dba1251260c04eebcb0125cb6
mwblevins <michaelwblevins@gmail.com>
2023-01-30 01:21:27 +0000

    blank space

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,6 +7,6 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         return class_to_json(self)
