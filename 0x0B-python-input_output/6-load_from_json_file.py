29604472a2e8aa4c0facba819f8cd971497fd007
mwblevins <michaelwblevins@gmail.com>
2023-01-30 00:50:31 +0000

    task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Task 6"""
+import json
+
+
+def load_from_json_file(filename):
+    with open(filename, 'r') as f:
+        contents = f.read()
+        obj = json.loads(contents)
+        return obj
