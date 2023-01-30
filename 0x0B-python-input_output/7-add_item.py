03b72d292c0cc082a9a6cbe8819ac2e0c1af19b6
mwblevins <michaelwblevins@gmail.com>
2023-01-30 01:05:38 +0000

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Task 7"""
+import sys
+import json
+
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+
+
+def add_items():
+    items = []
+    for item in sys.argv[1:]:
+        items.append(item)
+
+    with open("add_item.json", "w") as f:
+        json.dump(items, f)
+
+if __name__ == "__main__":
+    add_items()
