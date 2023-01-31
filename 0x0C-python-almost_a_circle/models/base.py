447b3a1b51fdebbb7a14ac76d47400631e45d0df
mwblevins <michaelwblevins@gmail.com>
2023-01-31 20:31:36 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,7 +17,46 @@ class Base():
 
 @staticmethod
 def to_json_string(list_dictionaries):
-    """Json strings"""
     if list_dictionaries is None or list_dictionaries == []:
         return "[]"
     return json.dumps(list_dictionaries)
+
+
+@classmethod
+def save_to_file(cls, list_objs):
+    with open((cls.__name__ + ".json"), 'w') as jasonfile:
+        if list_objs is None:
+            jasonfile.write("[]")
+        else:
+            richards = [ii.to_dictionary() for ii in list_objs]
+            jasonfile.write(Base.to_json_string(richards))
+
+
+@staticmethod
+def from_json_string(json_string):
+    if json_string == "[]" or json_string is None:
+        return []
+    else:
+        return json.loads(json_string)
+
+
+@classmethod
+def create(cls, **dictionary):
+    if dictionary:
+        if cls.__name__ == "Square":
+            nrich = cls(1)
+        elif cls.__name__ == "Rectangle":
+            nrich = cls(1, 1)
+        nrich.update(**dictionary)
+        return nrich
+
+
+@classmethod
+def load_from_file(cls):
+    fname = cls.__name__ + ".json"
+    try:
+        with open(fname) as jason:
+            jay = Base.from_json_string(jason.read())
+            return [cls.create(**x) for x in jay]
+    except IOError:
+        return []
