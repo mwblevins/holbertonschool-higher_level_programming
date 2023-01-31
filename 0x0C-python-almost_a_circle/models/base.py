7209d05e84ae2c83a17850da2da416b7fdd964bb
mwblevins <michaelwblevins@gmail.com>
2023-01-31 20:18:54 +0000

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """First class Base"""
+import json
 
 
 class Base():
@@ -12,3 +13,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+def to_json_string(list_dictionaries=[]):
+    """Json strings"""
+    list_dictionaries = list_dictionaries or []
+    return json.dumps(list_dictionaries)
