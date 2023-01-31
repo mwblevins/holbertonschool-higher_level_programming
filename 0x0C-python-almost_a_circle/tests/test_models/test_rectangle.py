04d9d18d69ef69a5a21fa45d83994c6a956ff72b
mwblevins <michaelwblevins@gmail.com>
2023-01-31 22:43:42 +0000

    last try

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""unit tests for rectangle.py"""
+"""unit tests for rectangle.py going crazy"""
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
