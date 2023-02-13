da801089a6d2985f20c30030213cea34a5676644
mwblevins <michaelwblevins@gmail.com>
2023-02-13 14:33:19 -0800

    comedy

---
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,5 @@
+-- hahaha
+SELECT title FROM tv_show_genres INNER JOIN tv_shows 
+ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres 
+ON genre_id = tv_genres.id
+WHERE name = "Comedy" ORDER BY title ASC;
