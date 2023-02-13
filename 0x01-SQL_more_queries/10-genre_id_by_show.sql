ae24bf38aaeb6cb67ee8381a2b09974cef8765f9
mwblevins <michaelwblevins@gmail.com>
2023-02-13 10:07:31 -0800

    reup

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,4 @@
 -- id of shows
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+FROM (tv_show_genres INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id) 
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
