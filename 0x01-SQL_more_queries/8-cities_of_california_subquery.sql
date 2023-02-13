6910ae964fe0cb852079f282ddda7135d4683129
mwblevins <michaelwblevins@gmail.com>
2023-02-13 09:47:12 -0800

    update again

--- 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x01-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,5 @@
 -- all cities of cali
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY ASC;
+SELECT cities.id, cities.name
+FROM cities
+WHERE cities.state_id = (SELECT id FROM states WHERE name = "California")
+ORDER BY cities.id ASC;
