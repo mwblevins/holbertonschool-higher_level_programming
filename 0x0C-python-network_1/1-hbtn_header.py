fa3b931049903f06202be0c97b713ba1bdba41e3
mwblevins <michaelwblevins@gmail.com>
2023-03-09 19:52:35 +0000

    task 1 update

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Response header value"""
+"""Response header header. Script that takes in a URL, 
+sends a request to the URL and displays the value of 
+the X-Request-Id variable found in the header of the response."""
 
 import urllib.request
-"""request url lib"""
 import sys
-"""import sys package"""
 
+if __name__ == "__main__":
 
-url = sys.argv[1]
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    headers = response.info()
-    x_request_id = headers['X-Request-Id']
-    print(x_request_id)
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        x_request_id = headers['X-Request-Id']
+        print(x_request_id)
