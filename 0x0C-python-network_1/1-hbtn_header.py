3bdebda250c41f9c639c600d7ea16a5e8191421c
mwblevins <michaelwblevins@gmail.com>
2023-03-09 19:54:27 +0000

    pep8

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Response header header. Script that takes in a URL, 
-sends a request to the URL and displays the value of 
+"""Response header header. Script that takes in a URL,
+sends a request to the URL and displays the value of
 the X-Request-Id variable found in the header of the response."""
 
 import urllib.request
