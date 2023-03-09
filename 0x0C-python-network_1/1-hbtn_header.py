83a6ade396337e58c667995731a67b2ee91db718
mwblevins <michaelwblevins@gmail.com>
2023-03-09 19:22:30 +0000

    task 1

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    headers = response.info()
+    x_request_id = headers['X-Request-Id']
+    print(x_request_id)
