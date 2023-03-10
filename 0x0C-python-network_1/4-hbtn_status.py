bcb20be4b87caac3fa6250b6e2cd9383a381edf1
mwblevins <michaelwblevins@gmail.com>
2023-03-10 20:49:19 +0000

    Task 4

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://intranet.hbtn.io/status
+You must use the package requests
+You are not allow to import packages other than requests"""
+
+import requests
+
+if __name__ == '__main__':
+    
+    url = 'https://intranet.hbtn.io/status'
+    response = requests.get(url)
+
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
