f0e2581ca4a636902d92594e5d038b4915d26e52
mwblevins <michaelwblevins@gmail.com>
2023-03-11 22:29:57 +0000

    task 9

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a Python script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display your id"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    auth = (username, token)
+    response = requests.get("https://api.github.com/user", auth=auth)
+    if response.status_code == 200:
+        data = response.json()
+        print("Your user ID is:", data["id"])
+    else:
+        print("Failed to retrieve user information. Status code:",
+              response.status_code)
