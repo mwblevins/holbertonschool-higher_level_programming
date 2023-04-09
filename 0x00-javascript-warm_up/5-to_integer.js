d6750619baeea6aae1acf4e66405b638305aba12
mwblevins <michaelwblevins@gmail.com>
2023-04-09 11:14:52 -0700

    SS

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const num = parseInt(process.argv[2], 10);
 if (Number.isInteger(num)) {
   console.log('My number: ' + num);
 } else {
-  console.log("Not a number");
+  console.log('Not a number');
 }
