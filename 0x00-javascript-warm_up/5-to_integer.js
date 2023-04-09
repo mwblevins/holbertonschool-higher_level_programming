81c29d6264bc0a35e35c2b397540ecc7764e4e81
mwblevins <michaelwblevins@gmail.com>
2023-04-09 11:13:33 -0700

    Task 5 updated

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // Print my number
-const num = parseInt(process.argv[2]);
-
-if (!isNaN(num)) {
-  console.log('My number: ${num}');
+const num = parseInt(process.argv[2], 10);
+if (Number.isInteger(num)) {
+  console.log('My number: ' + num);
 } else {
   console.log("Not a number");
 }
