75c48a9f3ee47051db108ad721326e4b928dd8f1
mwblevins <michaelwblevins@gmail.com>
2023-04-08 19:50:24 -0700

    Start for JS

---
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// Prints message depending on arguments passed
+const args = process.argv;
+if (args.length === 2) {
+    console.log('No argument');
+} else if (args.length === 3){
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
