8dbc2aa5d4accc684d9198e1fd1687bcabbe98c4
mwblevins <michaelwblevins@gmail.com>
2023-04-09 11:23:07 -0700

    SS

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints the same string multiple times
 const x = parseInt(process.argv[2], 10);
-if(Number.isNaN(x)) {
+if (Number.isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < x; i++) {
